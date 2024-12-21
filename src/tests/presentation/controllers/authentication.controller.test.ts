c05aa6620cc2cdab5361fe5d8f3191829701942c
Arthur Henrique Zarpelon Kunz <arthurhenriquezarpelonkunz@MacBook-Air-de-Arthur.local>
2024-12-21 10:53:06 -0300

    test: add status 500 if authentication throws

--- src/tests/presentation/controllers/authentication.controller.test.ts
+++ src/tests/presentation/controllers/authentication.controller.test.ts
@@ -1,12 +1,14 @@
 import { beforeEach, describe, expect, it } from 'vitest';
+import sinon from 'sinon';
 import { AuthenticationController } from '../../../server/presentation/controllers/authentication.controller';
 import { authenticationFakeInput } from '../../domain/authentication.fake';
+import { ServerError } from '../../../shared/errors/server-error.error';
 
 describe('AuthenticationController', () => {
-  let authenticationController: AuthenticationController;
+  let sut: AuthenticationController;
 
   beforeEach(() => {
-    authenticationController = new AuthenticationController();
+    sut = new AuthenticationController();
   });
   it('should return 400 if no email is provided', async () => {
     const httpRequest = {
@@ -14,7 +16,7 @@ describe('AuthenticationController', () => {
         password: authenticationFakeInput.password
       }
     };
-    const httpResponse = await authenticationController.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
   });
   it('should return 400 if no password is provided', async () => {
@@ -23,7 +25,22 @@ describe('AuthenticationController', () => {
         email: authenticationFakeInput.email
       }
     };
-    const httpResponse = await authenticationController.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+  });
+  it('should return 500 if an error occurs', async () => {
+    const httpRequest = {
+      body: {
+        email: authenticationFakeInput.email,
+        password: authenticationFakeInput.password
+      }
+    };
+    sinon.stub(sut, 'handle').throws(new ServerError('Internal server error'));
+    try {
+      await sut.handle(httpRequest);
+    } catch (error) {
+      expect(error).toBeInstanceOf(ServerError);
+    }
+    sinon.restore();
   });
 });
